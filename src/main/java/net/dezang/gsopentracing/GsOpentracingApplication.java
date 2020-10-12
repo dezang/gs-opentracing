70e0996649bd956e129b3cc050dfcf712edef6c6
Luke Hwang <luke@r2v.io>
2020-10-12 11:45:12 +0900

    Initial commit

---
+++ src/main/java/net/dezang/gsopentracing/GsOpentracingApplication.java
@@ -0,0 +1,13 @@
+package net.dezang.gsopentracing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsOpentracingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsOpentracingApplication.class, args);
+    }
+
+}
