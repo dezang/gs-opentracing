59bf4cc57ce3209c08fd0b05fdea486a9d002544
Luke Hwang <luke@r2v.io>
2020-10-13 13:56:02 +0900

    Add kafka producer app

--- README.md
+++ README.md
@@ -4,3 +4,26 @@
 - https://mvnrepository.com/artifact/io.jaegertracing/jaeger-client
 - https://mvnrepository.com/artifact/com.google.guava/guava
 
+# Producer App
+- https://github.com/opentracing-contrib/java-kafka-client
+- https://mvnrepository.com/artifact/io.opentracing.contrib/opentracing-kafka-client
+- https://mvnrepository.com/artifact/io.opentracing.contrib/opentracing-kafka-streams
+- https://mvnrepository.com/artifact/io.opentracing.contrib/opentracing-kafka-spring
+
+# Requirements
+## Kafka
+```shell script
+git clone git@github.com:confluentinc/cp-all-in-one.git
+cd cp-all-in-one
+cd cp-all-in-one
+docker-compose up -d
+```
+
+- http://localhost:9021/
+
+## jaeger
+```shell script
+docker run -d -p6831:6831/udp -p16686:16686 jaegertracing/all-in-one:latest
+```
+
+- http://localhost:16686
---
+++ app-producer/src/main/java/net/dezang/gsopentracing/ProducingApp.java
@@ -0,0 +1,70 @@
+package net.dezang.gsopentracing;
+
+import io.jaegertracing.internal.samplers.ConstSampler;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.kafka.spring.TracingProducerFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@EnableScheduling
+@SpringBootApplication
+public class ProducingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducingApp.class, args);
+    }
+
+    @Configuration
+    @RequiredArgsConstructor
+    static class Config {
+        @Value("${spring.application.name}")
+        private String applicationName;
+        private final KafkaProperties kafkaProperties;
+
+        @Bean
+        Tracer tracer() {
+            io.jaegertracing.Configuration.SamplerConfiguration samplerConfig = io.jaegertracing.Configuration.SamplerConfiguration.fromEnv().withType(ConstSampler.TYPE).withParam(1);
+            io.jaegertracing.Configuration.SenderConfiguration senderConfig = io.jaegertracing.Configuration.SenderConfiguration.fromEnv()
+                    .withAgentHost("localhost")
+                    .withAgentPort(6831);
+            io.jaegertracing.Configuration.ReporterConfiguration reporterConfig = io.jaegertracing.Configuration.ReporterConfiguration.fromEnv().withLogSpans(true).withSender(senderConfig);
+            io.jaegertracing.Configuration config = new io.jaegertracing.Configuration(applicationName)
+                    .withSampler(samplerConfig)
+                    .withReporter(reporterConfig);
+            return config.getTracer();
+        }
+
+        @Bean
+        ProducerFactory<String, String> producerFactory() {
+            return new TracingProducerFactory<>(new DefaultKafkaProducerFactory<>(
+                    kafkaProperties.buildProducerProperties()), tracer());
+        }
+
+        @Bean
+        KafkaTemplate<String, String> kafkaTemplate() {
+            return new KafkaTemplate<>(producerFactory());
+        }
+    }
+
+    @Component
+    @RequiredArgsConstructor
+    static class SimpleProducer {
+        private final KafkaTemplate<String, String> kafkaTemplate;
+
+
+        @Scheduled(fixedDelay = 10000)
+        public void produce() {
+            kafkaTemplate.send("test.tracing", "test");
+        }
+    }
+}
