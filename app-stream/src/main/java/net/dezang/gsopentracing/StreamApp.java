21001068c99e05a1ce1c9d000f69c8de3eae5c56
Luke Hwang <luke@r2v.io>
2020-10-13 20:33:33 +0900

    Tracing stream processing pipeline

--- app-stream/src/main/java/net/dezang/gsopentracing/StreamApp.java
+++ app-stream/src/main/java/net/dezang/gsopentracing/StreamApp.java
@@ -3,17 +3,25 @@ package net.dezang.gsopentracing;
 import io.jaegertracing.internal.samplers.ConstSampler;
 import io.opentracing.Scope;
 import io.opentracing.Span;
+import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
+import io.opentracing.contrib.kafka.TracingKafkaUtils;
 import io.opentracing.contrib.kafka.streams.TracingKafkaClientSupplier;
+import io.opentracing.util.GlobalTracer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaClientSupplier;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Printed;
+import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.Transformer;
+import org.apache.kafka.streams.processor.ProcessorContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,6 +32,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.Properties;
+import java.util.function.Function;
 
 @SpringBootApplication
 public class StreamApp {
@@ -54,6 +63,13 @@ public class StreamApp {
         KafkaClientSupplier kafkaClientSupplier() {
             return new TracingKafkaClientSupplier(tracer());
         }
+
+        @PostConstruct
+        public void registerToGlobalTracer() {
+            if (!GlobalTracer.isRegistered()) {
+                GlobalTracer.registerIfAbsent(tracer());
+            }
+        }
     }
 
     @Log4j2
@@ -71,14 +87,13 @@ public class StreamApp {
             streamProperties.putAll(kafkaProperties.buildStreamsProperties());
             StreamsBuilder streamsBuilder = new StreamsBuilder();
 
+            Serde<String> stringSerde = Serdes.String();
             KStream<String, String> stream = streamsBuilder.stream(
-                    "test.tracing", Consumed.with(Serdes.String(), Serdes.String()));
+                    "test.tracing", Consumed.with(stringSerde, stringSerde));
 
-            // TODO extractSpanContext
-            // TracingKafkaUtils.extractSpanContext(null, tracer);
-            stream.mapValues((key, value) -> work(value))
-                    .print(Printed.toSysOut());
-            stream.to("test.tracing.stream");
+            stream.transform(
+                    () -> new TransformerWithTracing<String, String>("work", new WorkFunction()))
+                    .to("test.tracing.stream", Produced.with(stringSerde, stringSerde));
 
             KafkaStreams streams = new KafkaStreams(streamsBuilder.build(), streamProperties, kafkaClientSupplier);
             streams.start();
@@ -100,6 +115,64 @@ public class StreamApp {
             } finally {
                 span.finish();
             }
+        }
+    }
+
+    static class WorkFunction implements Function<String, String> {
+
+        @Override
+        public String apply(String s) {
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return s + "worked";
+        }
+
+        @Override
+        public <V> Function<V, String> compose(Function<? super V, ? extends String> before) {
+            return null;
+        }
+
+        @Override
+        public <V> Function<String, V> andThen(Function<? super String, ? extends V> after) {
+            return null;
+        }
+    }
+
+    @Log4j2
+    static class TransformerWithTracing<V, VR> implements Transformer<String, V, KeyValue<String, VR>> {
+        private ProcessorContext context;
+        final Function valueAction;
+        final String operatorName;
+
+        public TransformerWithTracing(String operatorName, Function valueAction) {
+            this.operatorName = operatorName;
+            this.valueAction = valueAction;
+        }
+
+        @Override
+        public void init(ProcessorContext context) {
+            this.context = context;
+        }
+
+        @Override
+        public KeyValue<String, VR> transform(String key, V value) {
+            Tracer tracer = GlobalTracer.get();
+            Headers headers = context.headers();
+            SpanContext spanContext = TracingKafkaUtils.extractSpanContext(headers, tracer);
+            Span span = tracer.buildSpan(operatorName).asChildOf(spanContext).start();
+            try (Scope ignored = tracer.scopeManager().activate(span)) {
+                return KeyValue.pair(key, (VR) valueAction.apply(value));
+            } finally {
+                span.finish();
+            }
+        }
+
+        @Override
+        public void close() {
+
         }
     }
 }
